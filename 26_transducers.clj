de2b872af47fe95601e35ee6bdd9a5e94f442424
Graham@WSL <graham@ws.l>
2022-02-21 21:28:54 -0700

    Koan 26

--- 26_transducers.clj
+++ 26_transducers.clj
@@ -6,25 +6,35 @@
 
 (def transforms
   (comp (map inc)
-     (filter even?)))
+        (filter even?)))
+
+; Koan 26, transducers didn't really teach me much about transducers.
+; I would recommend independent study to understand what is actually
+; going on under the hood, because these problems don't really require
+; any understanding of the fundamentals!
 
 (meditations
  "A sequence operation with only one argument often returns a transducer"
- (= __
+;;  (= __
+ (= [2 3 4]
     (sequence example-transducer [1 2 3]))
 
  "Consider that sequence operations can be composed as transducers"
- (= __
+;;  (= __
+ (= [2 4]
     (transduce transforms conj [1 2 3]))
 
  "We can do this eagerly"
- (= __
+;;  (= __
+ (= [2 4]
     (into [] transforms [1 2 3]))
 
  "Or lazily"
- (= __
+;;  (= __
+ (= [2 4]
     (sequence transforms [1 2 3]))
 
  "The transduce function can combine mapping and reduction"
- (= __
+;;  (= __
+ (= 6
     (transduce transforms + [1 2 3])))
